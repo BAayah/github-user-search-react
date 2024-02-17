76cf3e3731fb289c06c12e293eda6e18a9c6aa5e
Aiat <138316674+BAayah@users.noreply.github.com>
2024-02-17 22:12:16 +0600

    first commit

---
+++ src/components/UserSearch.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { User } from './User';
+import { Form } from './Form';
+import { fetchResults } from './fetchResults';
+import '../style.css'
+
+export function UserSearch() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  function onSearchChange(event) {
+    setQuery(event.target.value);
+  }
+
+  async function onSearchSubmit(event) {
+    event.preventDefault();
+    const results = await fetchResults(query);
+    setResults(results);
+  }
+  
+  return (
+    <div className='search'>
+      <main className='main'>
+        <h2>GitHub User Search</h2>
+        <Form
+           onChange={onSearchChange}
+           onSubmit={onSearchSubmit}
+           value={query}
+        />
+        <h3>Results</h3>
+        <div id='results'>
+          <div>
+             {results.map((user) => (
+              <User 
+                 key={user.login}
+                 avatar={user.avatar_url}
+                 url={user.html_url}
+                 username={user.login}
+                 />
+             ))}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+export default UserSearch
+
